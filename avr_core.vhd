c6da6d0b6ef92353829fea10f6dbf298af8dfbde
YUKIHIRO Ishikawa <ykhr7121rkhy@gmail.com>
2017-09-20 01:28:20 +0900

    シミュレーション1回目、PCカウントせず

--- avr_core.vhd
+++ avr_core.vhd
@@ -7,20 +7,20 @@ use ieee.numeric_std.all;
 entity avr_core is
 	port(
 		reset_n : in std_logic;
-		clock : in std_logic;
-		out_addr_bus : out std_logic_vector(15 downto 0);
-		inout_data_bus : inout std_logic_vector(7 downto 0);
+		clock : in std_logic
+		--out_addr_bus : out std_logic_vector(15 downto 0);
+		--inout_data_bus : inout std_logic_vector(7 downto 0);
 	);
 end avr_core;
 
 architecture logic of avr_core is
-	type regarray is array (0 to 31) of (std_logic_vector(7 downto 0)); -- general purpose regs
-	type instructs is array (add,adc,adiw,sub,subi,sbc,sbci,sbiw,_and,andi,_or,ori,eor,com,neg,sbr,cbr,inc,dec,tst,clr,ser,mul,
+	type regarray is array (0 to 31) of std_logic_vector(7 downto 0); -- general purpose regs
+	type instructs is (add,adc,adiw,sub,subi,sbc,sbci,sbiw,and0,andi,or0,ori,eor,com,neg,sbr,cbr,inc,dec,tst,clr,ser,mul,
 									rjmp,ijmp,rcall,call,ret,reti,cpse,cp,cpc,cpi,sbrc,sbrs,sbic,sbis,brbs,brbc,breq,brne,brcs,brcc,brsh,brlo,brmi,brpl,brge,brlt,brhs,brhc,brts,brtc,brvs,brvc,brie,brid,
-									mov,ldi,lds,ld,ldd,sts,st,_std,lpm,_in,_out,push,pop,
-									lsl,lsr,_rol,_ror,asr,swap,bset,bclr,sbi,cbi,bst,bld,sec,clc,sen,cln,sez,clz,sei,cli,ses,cls,sev,clv,set,clt,seh,clh,nop,sleep,wdr);
-	type reg_8bit_array is array (0 to 15) of (std_logic_vector(7 downto 0));
-	type reg_16bit_array is array (0 to 15) of (std_logic_vector(15 downto 0));
+									mov,ldi,lds,ld,ldd,sts,st,std0,lpm,in0,out0,push,pop,
+									lsl,lsr,rol0,ror0,asr,swap,bset,bclr,sbi,cbi,bst,bld,sec,clc,sen,cln,sez,clz,sei,cli,ses,cls,sev,clv,set,clt,seh,clh,nop,sleep,wdr);
+	type reg_8bit_array is array (0 to 15) of std_logic_vector(7 downto 0);
+	type reg_16bit_array is array (0 to 15) of std_logic_vector(15 downto 0);
 	signal regs : regarray;
 	signal immidiate : reg_8bit_array;
 	signal reg_16buf : reg_16bit_array;
@@ -32,10 +32,12 @@ architecture logic of avr_core is
 	signal src_reg  : reg_8bit_array;
 	signal dest_reg : reg_16bit_array;
 	signal insts : reg_16bit_array;
-	signal reg_i : instructs;
+	signal reg_inst : instructs;
 	signal pipeline_counter : integer range 0 to 3;
 	signal counter : integer range 0 to 15;
 	signal tmp0,tmp1,tmp2,tmp3,tmp4 : reg_16bit_array;
+	signal inst_mem_addr_0,inst_mem_addr_1 : std_logic_vector(8 downto 0);
+	signal inst_mem_out_0,inst_mem_out_1 : std_logic_vector(7 downto 0);
 	component multiply is
 		PORT
 		(
@@ -65,9 +67,18 @@ begin
 			datab => multb,
 			result => mult_out
 		);
+
+	mem : inst_mem
+		port map(
+			address_a => inst_mem_addr_0,
+			address_b => inst_mem_addr_1,
+			clock => clock,
+			q_a => inst_mem_out_0,
+			q_b => inst_mem_out_1
+		);
 			
-	inst_mem_addr_0 <= pc & '0';
-	inst_mem_addr_1 <= pc & '1';
+	inst_mem_addr_0 <= std_logic_vector(to_unsigned(pc,8)) & '0';
+	inst_mem_addr_1 <= std_logic_vector(to_unsigned(pc,8)) & '1';
 	insts(0) <= inst_mem_out_1 & inst_mem_out_0;
 	
 	process (reset_n,clock) begin
@@ -77,17 +88,19 @@ begin
 			else
 				pc <= pc + 1;
 				for j in 0 to 14 loop
-					insts(i+1) <= insts(i);
+					insts(j+1) <= insts(j);
 				end loop;
 				for i in 0 to 15 loop
-					case insts(i)
+					case insts(i) is
 						when "000011----------"	=> --add
+							reg_inst <= add;
 							dest_reg(i)(7 downto 0) <= regs(to_integer(unsigned(insts(i)(8 downto 4))));
 							src_reg(i) <= regs(to_integer(unsigned(insts(i)(9) & insts(i)(3 downto 0))));
 							tmp0(i) <= std_logic_vector(unsigned(dest_reg(i)(7 downto 0)) + to_integer(unsigned(src_reg(i))));
 							tmp1(i) <= tmp0(i);
 							regs(to_integer(unsigned(insts(i)(8 downto 4)))) <= tmp1(i);
 						when "10010110--------" => --adiw
+							reg_inst <= adiw;
 							dest_reg(i)(7 downto 0) <= regs(to_integer(unsigned(24+unsigned(insts(i)(5 downto 4) & '0'))));
 							dest_reg(i)(15 downto 8) <= regs(to_integer(unsigned(24+unsigned(insts(i)(5 downto 4) & '1'))));
 							immidiate(i) <= insts(i)(7 downto 6) & insts(i)(3 downto 0);
@@ -96,36 +109,45 @@ begin
 							regs(to_integer(unsigned(24+unsigned(insts(i)(5 downto 4) & '1')))) <= tmp2(i)(15 downto 8);
 							regs(to_integer(unsigned(24+unsigned(insts(i)(5 downto 4) & '0')))) <= tmp2(i)(7 downto 0);	
 						when "000110----------" =>--sub
+							reg_inst <= sub;
 							dest_reg(i)(7 downto 0) <= regs(to_integer(unsigned(insts(i)(8 downto 4))));
 							src_reg(i) <= regs(to_integer(unsigned(insts(i)(9) & insts(i)(3 downto 0))));
 							tmp1(i) <= std_logic_vector(unsigned(dest_reg(i)(7 downto 0)) - to_integer(unsigned(src_reg(i))));
 							tmp2(i) <= tmp1(i);
 							regs(to_integer(unsigned(insts(i)(8 downto 4)))) <= tmp2(i); 
 						when "1001010-----0000" => --com
+							reg_inst <= com;
 							tmp0(i)(7 downto 0) <= regs(to_integer(unsigned(insts(i)(8 downto 4))));
 							tmp1(i) <= tmp0(i);
 							tmp2(i) <= tmp1(i);
 							regs(to_integer(unsigned(insts(i)(8 downto 4)))) <= std_logic_vector(X"FF" - unsigned(tmp2(i)(7 downto 0)));
 						when "100111----------" => --mul
-							mult_out <= others => '0';
+							reg_inst <= mul;
+							mult_out <= (others => '0');
 							multa <= regs(to_integer(unsigned(insts(i)(8 downto 4))));
-							multb <= regs(to_integer(unsigned(insts(i)(9)) & unsigned(insts(i)(3 downto 0))));
-							regs(to_integer(unsigned(insts(i)(8 downto 4)))) <= mult_out;
+							multb <= regs(to_integer(unsigned(insts(i)(9) & insts(i)(3 downto 0))));
+							regs(1) <= mult_out(15 downto 8);
+							regs(0) <= mult_out(7 downto 0);
 						when "1110------------" => --ldi
+							reg_inst <= ldi;
 							tmp0(i) <= insts(i)(11 downto 8) & insts(i)(3 downto 0);
 							tmp1(i) <= tmp0(i);
 							tmp2(i) <= tmp1(i);
-							regs(to_integer(unsigned(insts(i)(7 downto 4) + 16))) <= tmp2(i);
+							regs(to_integer(unsigned(insts(i)(7 downto 4)) + 16)) <= tmp2(i);
 						when "1100------------" => --rjmp
+							reg_inst <= rjmp;
 							tmp0(i) <= std_logic_vector(pc + signed(insts(i)(11 downto 0)));
 							tmp1(i) <= tmp0(i);
 							tmp2(i) <= tmp1(i);
-							pc <= tmp2(i);
+							pc <= to_integer(unsigned(tmp2(i)));
 						when "0000000000000000" =>  --nop
+							reg_inst <= nop;
 							tmp1(i) <= tmp0(i);
 							tmp2(i) <= tmp1(i);
 							tmp3(i) <= tmp2(i);
 							tmp4(i) <= tmp3(i);
+						when others =>
+							null;
 					end case;
 				end loop;
 			end if;
